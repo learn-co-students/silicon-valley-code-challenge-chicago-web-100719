21daa7084f4689c5b89d4c2cb653aea9c7f3b536
Janae Hall <janaeelisa@gmail.com>
2019-10-11 15:58:59 -0500

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,24 @@
-class FundingRound
 
+class FundingRound
+    
+    attr_accessor :type
+    attr_reader :startup, :venture_capitalist, :investment
+    
+    @@all = []
+
+    def initialize(startup, venture_capitalist)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @@all << self
+    end
+
+    def investment=(float)
+        if float.class == Float && float.positive?
+            @investment = float
+        end
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,59 @@
 class Startup
+    
+    attr_accessor :name
+    attr_reader :founder, :domain
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(name)
+        self.all.find do |s|
+            s.founder == name
+        end
+    end
+
+    def self.domains
+        self.all.collect do |s|
+            s.domain
+        end
+    end
+
+    def sign_contract(venture_capitalist, type, amount)
+        fund = FundingRound.new(self, venture_capitalist)
+        fund.type = type
+        fund.investment = amount
+    end
+
+    def num_funding_rounds
+        FundingRound.all.select {|f| f.startup == self}.length
+    end
+
+    def investors
+        FundingRound.all.select {|f| f.startup == self}.collect {|v| v.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        self.investors.select {|i| VentureCapitalist.tres_commas_club.include?(i)}.uniq
+    end
+
+    def total_funds
+        fund = FundingRound.all.select {|f| f.startup == self}
+        fund.sum {|f| f.investment}
+    end
 
 end
