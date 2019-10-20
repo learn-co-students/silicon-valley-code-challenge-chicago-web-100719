8c17d0a76cf52fc169f75ec7befa8b3895fd6d36
Valeria <valeria.gutsuliak@gmail.com>
2019-10-20 18:17:24 -0500

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,21 @@
+require_relative 'startup'
+require_relative 'venture_capitalist'
+
 class FundingRound
+    attr_reader :startup, :venture_capitalist, :type, :investment
+
+    @@all = []
+
+    def initialize(startup,venture_capitalist, type, investment)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type 
+        @investment = investment
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+google = Startup.new("Google", "Nik", "search")
+airbnb = Startup.new("airbnb", "Dan", "rent")
+
+
+
+pat = VentureCapitalist.new("Pat", 1000000000)
+val = VentureCapitalist.new("Val", 10000000)
+
+
+googlepat = FundingRound.new(google, pat, "seriesA", 1000000)
+googleval = FundingRound.new(google, val, "serieaB", 10000)
+
+airbnbpat = FundingRound.new(airbnb, pat, "seriesC", 1000000000)
+airbnbval = FundingRound.new(airbnb, val, "seriesD", 100000)
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
